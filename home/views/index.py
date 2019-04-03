f44226bd74902b82019ece03f417e07aeb9aaf26
Rafiul Islam <avoidcloud@gmail.com>
2019-04-03 15:50:29 +0600

    building...

--- home/views/index.py
+++ home/views/index.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, redirect
 
-from home.models import Spot
+from home.models import Spot, Event
 
 
 def view(request):
 	context = {}
 	spots = Spot.objects.all()[:6]
+	events = Event.objects.all()[:6]
+
 	context['spots'] = spots
+	context['events'] = events
+	
 	return render(request, 'home/index.html', context)
